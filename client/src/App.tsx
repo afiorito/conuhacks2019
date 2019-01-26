a3c46b62c8571f4067710df003e4a610a0f9ecd1
afiorito <fioritoanthony@outlook.com>
2019-01-26 14:41:44 -0500

    updated boilerplate for typescript and sass

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
+import './App.scss';
 import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
-  render() {
+  public render() {
     return (
       <div className="App">
         <header className="App-header">
