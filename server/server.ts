efdee444cb3289f9801c84432949b09535f88622
afiorito <fioritoanthony@outlook.com>
2019-01-26 15:35:08 -0500

    added express body parser

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,8 @@
+import * as bodyParser from 'body-parser';
 import express from 'express';
 
 const app = express();
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
