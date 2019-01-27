e59bf77ccf76f08b3fda0ce2c70dd59044b8cb03
afiorito <fioritoanthony@outlook.com>
2019-01-27 01:29:26 -0500

    added camera system

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
+import { Container, Input } from 'reactstrap';
 import './App.scss';
-import logo from './logo.svg';
+import camera from './camera.svg';
+import { GBNavbar } from './GBNavbar';
 
 class App extends Component {
+  private photoInput = React.createRef<HTMLInputElement>();
+
+  public accessCamera = () => {
+    if (this.photoInput.current) {
+      this.photoInput.current.click();
+    }
+  }
+
   public render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <GBNavbar/>
+        <Container>
+          <div className="input-container">
+            <Input placeholder="Search for a sustainable alternative..." />
+            <div onClick={this.accessCamera} className="camera-input">
+              <input ref={this.photoInput} type="file" accept="image/*" capture="camera" />
+              <img src={camera} />
+            </div>
+          </div>
+        </Container>
       </div>
     );
   }
---
+++ client/src/GBNavbar.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import {
+  Collapse,
+  Nav,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  NavItem,
+  NavLink } from 'reactstrap';
+import './GBNavbar.scss';
+
+interface IGBNavbarState {
+  isOpen: boolean;
+}
+
+export class GBNavbar extends React.Component<{}, IGBNavbarState>  {
+  constructor(props: object) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  public toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+  public render() {
+    return (
+      <div>
+        <Navbar className="GBNavbar" expand="md">
+          <NavbarBrand href="/">Green Beans</NavbarBrand>
+        </Navbar>
+      </div>
+    );
+  }
+}
