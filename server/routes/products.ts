4d1109b9e47eb647d966abc1ed7806b2e399bd14
mg <marialgracedeleon@gmail.com>
2019-01-27 07:26:19 -0500

    Added image recognition

--- server/routes/products.ts
+++ server/routes/products.ts
@@ -5,37 +5,37 @@ const products = express.Router();
 
 const upload = multer();
 
-async function quickstart() {
-    // Imports the Google Cloud client library
-    const vision = require('@google-cloud/vision');
-  
-    // Creates a client
-    const client = new vision.ImageAnnotatorClient();
-    const [result] = await client.webDetection('C:/Users/Marial Grace/Desktop/pics/knife.jpg');
-    const webDetection = result.webDetection;
-  
-    if (webDetection.webEntities.length) {
-      console.log(`Web entities found: ${webDetection.webEntities.length}`);
-      webDetection.webEntities.forEach((webEntity:any) => {
-        console.log(`  Description: ${webEntity.description}`);
-        console.log(`  Score: ${webEntity.score}`);
-      });
-     }
-    return webDetection;
-  }
-  
-  (async function() {
-      const result = await quickstart();
-  
-      console.log(result);
-  })();
-
 products.get('/', (req, res) => {
     res.json([]);
 });
 
 products.post('/search/image', upload.array('file'), (req, res) => {
-    console.log(req);
+    const file = Object.values(req.files)[0];
+    console.log(file.buffer);
+    quickstart(file.buffer);
+    //console.log(file);
 });
 
 export { products };
+
+async function quickstart(temp:any) {
+    // Imports the Google Cloud client library
+    const vision = require('@google-cloud/vision');
+  
+    // Convert the image data to a Buffer and base64 encode it.
+    var encoded = Buffer.from(temp).toString('base64');
+
+    // Creates a client
+    const client = new vision.ImageAnnotatorClient();
+
+    const [result] = await client.labelDetection({image: { content: encoded}});
+    const labels = result.labelAnnotations;
+
+    var words=[]; 
+  
+    // labels.forEach((label:any) => console.log(label.description));
+    var i;
+    for (i = 0; i < labels.length; i++)
+        words[i] = labels[i].description;
+    console.log(words);
+}
