7d58cb8f089169d707d07290df48bf72d4df3373
mg <marialgracedeleon@gmail.com>
2019-01-27 04:39:49 -0500

    Removed key

--- server/routes/products.ts
+++ server/routes/products.ts
@@ -5,6 +5,31 @@ const products = express.Router();
 
 const upload = multer();
 
+async function quickstart() {
+    // Imports the Google Cloud client library
+    const vision = require('@google-cloud/vision');
+  
+    // Creates a client
+    const client = new vision.ImageAnnotatorClient();
+    const [result] = await client.webDetection('C:/Users/Marial Grace/Desktop/pics/knife.jpg');
+    const webDetection = result.webDetection;
+  
+    if (webDetection.webEntities.length) {
+      console.log(`Web entities found: ${webDetection.webEntities.length}`);
+      webDetection.webEntities.forEach((webEntity:any) => {
+        console.log(`  Description: ${webEntity.description}`);
+        console.log(`  Score: ${webEntity.score}`);
+      });
+     }
+    return webDetection;
+  }
+  
+  (async function() {
+      const result = await quickstart();
+  
+      console.log(result);
+  })();
+
 products.get('/', (req, res) => {
     res.json([]);
 });
