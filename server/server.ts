bb5af15c032422afe7e72186fe1942a326f372ac
fozahm <fozail_113@sympatico.ca>
2019-01-26 22:48:36 -0500

    Added port variable

--- server/server.ts
+++ server/server.ts
@@ -2,12 +2,14 @@ import * as bodyParser from 'body-parser';
 import express from 'express';
 
 const app = express();
+var port:number = 80;
+
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.listen(5000, () => {
-    console.log(`Server listening on port 5000`);
+app.listen(port, () => {
+    console.log(`Server listening on port ` + port);
 });
